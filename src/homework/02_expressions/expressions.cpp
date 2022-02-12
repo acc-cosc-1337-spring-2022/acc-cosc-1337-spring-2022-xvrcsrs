f3f54b1480ff7337f1db9dbf86c776aaf612a0a0
Xavier Casares <francisco.casares@g.austincc.edu>
2022-02-11 20:17:59 -0600

    hw 2 w test cases

--- src/homework/02_expressions/expressions.cpp
+++ src/homework/02_expressions/expressions.cpp
@@ -1,4 +1,5 @@
 #include "expressions.h"
+const double TAX_RATE = 0.0675;
 
 //example
 int add_numbers(int num1, int num2)
@@ -9,6 +10,14 @@ int add_numbers(int num1, int num2)
 //write code for multiply_numbers function here
 //function returns product of num1 and num2
 
+double get_sales_tax_amount(double meal_amount)
+{
+	return meal_amount * TAX_RATE;
+}
 
 
-
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	tip_rate = tip_rate / 100;
+	 return meal_amount * tip_rate;
+}
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,46 @@
 ﻿//write include statements
+#include "expressions.h"
+#include<iostream>
 
 
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	//initialize variables
+	double meal_amount;
+	double tip_rate;
+	double tip_amount;
+	double tax_amount;
+	double total;
+
+	//Ask for meal amount
+	cout<<"How much was your meal? ";
+	cin>>meal_amount;
+
+	//Calculate tax
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	//Ask for tip percentage
+	cout<<"What percentage would you like to tip? ";
+	cin>>tip_rate;
+
+	//Get tip amount
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	//Get total
+	total = meal_amount + tip_amount + tax_amount;
+
+	//Print results
+	cout<<"Meal Amount: "<<meal_amount<<"\n";
+	cout<<"Sales Tax: "<<tax_amount<<"\n";
+	cout<<"Tip Amount: "<<tip_amount<<"\n";
+	cout<<"Total: "<<total<<"\n";
 
 	return 0;
 }
