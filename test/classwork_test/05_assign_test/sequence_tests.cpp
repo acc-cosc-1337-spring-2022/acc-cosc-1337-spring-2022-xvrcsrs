b0990b63fd1eafda7bdc014ec09883c9546b4476
Xavier Casares <francisco.casares@g.austincc.edu>
2022-03-05 21:16:50 -0600

    hw 5 w test case

--- test/classwork_test/05_assign_test/sequence_tests.cpp
+++ test/classwork_test/05_assign_test/sequence_tests.cpp
@@ -1,8 +1,27 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "loops.h"
+#include "sequence.h"
+#include<string>
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Verify GC Content")
+{
+	REQUIRE(get_gc_content("AGCTATAG") == 0.375);
+	REQUIRE(get_gc_content("CGCTATAG") == 0.50);
+}
+
+TEST_CASE("Verify DNA Complement")
+{
+	REQUIRE(get_dna_complement("AAAACCCGGT")=="ACCGGGTTTT");
+	REQUIRE(get_dna_complement("CCCGGAAAAT")=="ATTTTCCGGG");
+}
+
+TEST_CASE("Verify Reverse String")
+{
+	REQUIRE(reverse_string("AGCTATAG")=="GATATCGA");
+	REQUIRE(reverse_string("CGCTATAG")=="GATATCGC");
+}
