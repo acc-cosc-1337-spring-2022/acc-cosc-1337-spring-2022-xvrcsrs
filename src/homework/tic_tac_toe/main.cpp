998ceb088edea7a9031662e70dd6cb8abea44032
Xavier Casares <francisco.casares@g.austincc.edu>
2022-03-26 20:42:31 -0500

    hw 6 w test cases

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,36 @@
+#include "tic_tac_toe.h"
+
+using std::cout; using std::cin; using std::string;
 
 int main() 
 {
+    TicTacToe game;
+    string first_player;
+    char choice;
 
-	return 0;
+    do
+    {
+    
+    cout<<"Enter first player or (X or O)";
+    cin>>first_player;
+	game.start_game(first_player);
+
+    int position;
+    while(game.game_over() == false)
+    {
+        cout<<"Enter position [1-9]";
+        cin>>position;
+        game.mark_board(position);
+        game.display_board();
+    }
+
+    cout<<"Continue type Y: ";
+    cin>>choice;
+
+
+        /* code */
+    } while (choice == 'Y' || choice == 'y');
+    
+    
+    return 0;
 }
